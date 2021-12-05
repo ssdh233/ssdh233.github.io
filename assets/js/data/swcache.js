093a3d2bb15a6b5333063be518855e9bf7c3482d
ssdh233 <songsongdahu233@gmail.com>
2021-12-06 05:39:50 +0900

    Update

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -29,11 +29,6 @@ const resource = [
   /* --- Favicons --- */
   {% assign favicon_path = "/assets/img/favicons" | relative_url %}
 
-  '{{ favicon_path }}/android-chrome-192x192.png',
-  '{{ favicon_path }}/android-chrome-512x512.png',
-  '{{ favicon_path }}/apple-touch-icon.png',
-  '{{ favicon_path }}/favicon-16x16.png',
-  '{{ favicon_path }}/favicon-32x32.png',
   '{{ favicon_path }}/favicon.ico',
   '{{ favicon_path }}/mstile-150x150.png',
   '{{ favicon_path }}/site.webmanifest',
